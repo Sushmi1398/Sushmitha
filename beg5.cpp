f430eb2c4634ac5aff5e7fe7319ce4dea5fb5644
Sushmi1398 <53041950+Sushmi1398@users.noreply.github.com>
2019-07-24 14:12:53 +0530

    Create beg5.cpp

---
+++ beg5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	int a,b,c
+	if(a>b)
+	{
+		if (c>a)
+		  cout<<c;
+		else
+		  cout<<a;
+	}
+	else 
+	{
+		if (b>c)
+			cout<<b;
+		else
+			cout<<c;
+	}
+	return 0;
+}
