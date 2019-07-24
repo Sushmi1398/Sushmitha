ed7503c339faf10d421882e5b36817ca1693ed50
Sushmi1398 <53041950+Sushmi1398@users.noreply.github.com>
2019-07-24 14:19:42 +0530

    Create beg9.cpp

---
+++ beg9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,sum=0;
+	cin>>a>>b;
+	for (int i=0;i<a; i++)
+	{
+		cin>>a[i];
+		if(i<k)
+		sum+=a[i];
+	}
+	return 0;
+}
