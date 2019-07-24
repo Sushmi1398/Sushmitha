ea8e7a5d906005d830a558bedf3904b3ec03b55b
Sushmi1398 <53041950+Sushmi1398@users.noreply.github.com>
2019-07-24 14:05:08 +0530

    Create beg7.cpp

---
+++ beg7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	int i=0;
+	while (i<n)
+	  {cout<<"Hello\n";
+	   i++;
+	  }
+	
+	return 0;
+}
